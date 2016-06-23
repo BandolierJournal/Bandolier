46d358d46884def38b7664e0f65e5b926c6135db
sechu <sabrina.chu@gmail.com>
2016-06-23 12:59:41 -0400

    bower/index scaffold

---
+++ app.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const app = angular.module('journal', ['ui.router']);
+
--- main.js
+++ main.js
@@ -6,29 +6,26 @@ const {BrowserWindow} = electron;
 const ipcMain = electron.ipcMain;
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win;
+let mainWindow;
 let onlineStatusWindow;
 
 function createWindow() {
-  win = new BrowserWindow({width: 800, height: 600});
-  win.loadURL(`file://${__dirname}/index.html`);
-  // Open the DevTools.
-  win.webContents.openDevTools();
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  // win.webContents.openDevTools(); //DevTools
 
   // Emitted when the window is closed.
-  win.on('closed', () => {
+  mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
+    mainWindow = null;
   });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
 app.on('ready', () => {
+  createWindow();
   onlineStatusWindow = new BrowserWindow({width: 0, height: 0, show: false});
   onlineStatusWindow.loadURL(`file://${__dirname}/online-status.html`)
 });
