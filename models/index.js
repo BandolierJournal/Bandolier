466483bbcd9545a645b1a449d31f37a61246da69
batmantis <batmantisex@gmail.com>
2016-06-23 12:08:58 -0400

    working on seed

--- models/index.js
+++ models/index.js
@@ -1,13 +1,15 @@
 var PouchDB = require('pouchdb');
-
+PouchDB.plugin(require('relational-pouch'))
 var db = new PouchDB('bullet');
+
+var remoteDB = new PouchDB('http://localhost:5984/bullet')
 
 db.setSchema([
     {
         singular: 'collection',
         plural: 'collections',
         relations: {
-            bullet: {hasMany: 'bullet'}
+            'bullets': {hasMany: 'bullet'}
         }
     }
 ]);
