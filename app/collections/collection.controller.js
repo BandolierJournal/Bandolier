932008c3fbda60dd16cf7dc87e7cf1e823ad3544
Octowl <alsaff1987@gmail.com>
2016-06-23 19:51:59 -0400

    Remove testing tag

--- app/collections/collection.controller.js
+++ app/collections/collection.controller.js
@@ -1,5 +1,5 @@
 bulletApp.controller('CollectionCtrl', function($scope, $log, Collection){
-    Collection.fetchById(5)
+    Collection.fetchById(5) // TODO: This should be more dynamic
     .then(function(res){
         angular.extend($scope, res);
         $scope.$evalAsync();
