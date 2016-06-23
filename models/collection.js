508ca63ea3cf901703b67be9cbde67a7efdd75bc
Octowl <alsaff1987@gmail.com>
2016-06-23 11:35:22 -0400

    Create collection constructor

--- models/collection.js
+++ models/collection.js
@@ -1,1 +1,8 @@
-// TODO: collection definition
+/*jshint node: true*/
+
+var Collection = function(title) {
+    this.id = new Date().toISOString();
+    this.title = title;
+};
+
+modules.export = Collection;
