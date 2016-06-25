3792106159c104d92be181a360a89970d048b2a0
Octowl <alsaff1987@gmail.com>
2016-06-25 16:30:55 -0400

    Implement crossing out and disable bullets when crossed out or done

--- app/bullets/bullet.directive.js
+++ app/bullets/bullet.directive.js
@@ -16,14 +16,18 @@ bulletApp.directive('bullet', function (Bullet) {
             let map = {};
 
             function editBullet() {
-                // cmd-t change to task
-                if (map[84]) return new Bullet.Task(scope.bullet);
-                // cmd-e change to event
-                if (map[69]) return new Bullet.Event(scope.bullet);
-                // cmd-n change to note
-                if (map[78]) return new Bullet.Note(scope.bullet);
-                // cmd-d toggle done for tasks
-                if (map[68] && scope.bullet.type === 'Task') scope.bullet.toggleDone();
+                if(!scope.bullet.strike) {
+                    if(!scope.bullet.status || scope.bullet.status === 'incomplete') {
+                        // cmd-t change to task
+                        if (map[84]) return new Bullet.Task(scope.bullet);
+                        // cmd-e change to event
+                        if (map[69]) return new Bullet.Event(scope.bullet);
+                        // cmd-n change to note
+                        if (map[78]) return new Bullet.Note(scope.bullet);
+                    }
+                    // cmd-d toggle done for tasks
+                    if (map[68] && scope.bullet.type === 'Task') scope.bullet.toggleDone();
+                }
                 // cmd-x cross out
                 if (map[88]) scope.bullet.toggleStrike();
                 return scope.bullet;
@@ -35,7 +39,7 @@ bulletApp.directive('bullet', function (Bullet) {
 
             element.on('keydown', function (e) {
                 map[e.which] = true;
-                // console.log(map);
+
                 if (map[13]) {
                     map = {};
                     e.preventDefault();
@@ -43,7 +47,11 @@ bulletApp.directive('bullet', function (Bullet) {
                 } else if (map[91]) {
                     scope.bullet = editBullet();
                     scope.bullet.save();
+                } else if(scope.bullet.strike || scope.bullet.status === 'complete') {
+                    map = {};
+                    e.preventDefault();
                 }
+
             });
 
             element.on('focusout', function (e) {
