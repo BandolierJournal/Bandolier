6d9a9af5cc0fc1143303bd013b8f2f5aa62194a6
sechu <sabrina.chu@gmail.com>
2016-06-28 13:34:14 -0400

    css fixes, mostly grid

--- app/scripts/generic/generic.controller.js
+++ app/scripts/generic/generic.controller.js
@@ -2,3 +2,8 @@ bulletApp.controller('GenericCtrl', function ($scope, collection) {
   $scope.collection = collection.collection; // this is why i dislike our fetchById fxn
 });
 
+
+
+//prettify html
+//consistent semicolon
+//eslintrc
