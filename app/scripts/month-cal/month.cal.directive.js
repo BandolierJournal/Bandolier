5fde170b79012fb96b4cdcf1e1c95772ce28d868
batmantis <batmantisex@gmail.com>
2016-06-28 14:46:50 -0400

    created monthCal directive

---
+++ app/scripts/month-cal/month.cal.directive.js
@@ -0,0 +1,69 @@
+/*jshint esversion: 6*/
+
+bulletApp.directive('monthCal', function($log){
+    return {
+        restrict: 'E',
+        templateUrl: 'scripts/month-cal/month.cal.template.html',
+        scope: {
+            collectionId: '@',
+            props: '='
+        },
+        link: function(scope) {
+            Collection.fetchById(scope.collectionId)
+            .then(function(res){
+                angular.extend(scope, res);
+                scope.formattedTitle = formatTitle(scope.collection);
+                scope.$evalAsync();
+            })
+            .catch(function(err) {
+                scope.collection = new Collection(scope.props);
+                scope.formattedTitle = formatTitle(scope.collection);
+                scope.$evalAsync();
+            });
+
+            scope.newBullet = new Bullet.Task()
+
+            function formatTitle(collection) {
+                switch(collection.type) {
+                    case 'month':
+                        return Moment(collection.title).format('MMMM')+' Log';
+                        break;
+                    case 'future':
+                        return Moment(collection.title).format('MMM YY').toUpperCase();
+                        break;
+                    case 'day':
+                        return Moment(collection.title).format('MMM DD');
+                        break;
+                    default:
+                        return collection.title;
+                }
+
+            }
+
+
+            /**********************************************************
+            * This function will remove the bullet from the collection
+            * and then make sure the bullet is also removed from the
+            * local bullets array.
+            **********************************************************/
+            scope.removeBullet = function(bullet) {
+                scope.collection.removeBullet(bullet)
+                .then(function(){
+                    scope.bullets = scope.bullets.filter(b => b.id !== bullet.id);
+                })
+                .catch($log.err);
+            };
+            scope.addBullet = function(bullet) {
+                if (bullet.content.length > 0) {
+                  scope.collection.addBullet(bullet)
+                  .then(function(){
+                      scope.bullets.push(bullet);
+                      scope.newBullet = new Bullet.Task()
+                      scope.$evalAsync()
+                  })
+                  .catch($log.err);
+              };
+            }
+        }
+    };
+});
