aec80d7750760fa15298f92131bba34effd8c2be
batmantis <batmantisex@gmail.com>
2016-06-28 21:39:56 -0400

    fixed id to be correct

--- app/scripts/monthlytracker/monthlytracker.controller.js
+++ app/scripts/monthlytracker/monthlytracker.controller.js
@@ -1,6 +1,5 @@
 bulletApp.controller('MonthlyTrackerCtrl', function($scope, targetMonth, numOfDays) {
   $scope.numOfDays = numOfDays;
-  debugger;
   var month = targetMonth[0].collection.title;   // eventually  let month = targetMonth[0].title;
   $scope.log = targetMonth.find(i => i.collection.type==="month") || new Collection(month, 'month');  //eventually $scope.log = targetMonth.find(i => i.type==="month") || new Collection(month, 'month');
   $scope.cal = targetMonth.find(i => i.collection.type==="month-cal") || new Collection(month, 'month-cal');
