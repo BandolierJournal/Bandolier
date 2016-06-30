982251cdc7147ca366c75ea67be87ff0739b84c9
batmantis <batmantisex@gmail.com>
2016-06-29 20:15:48 -0400

    nav bar states persist

--- app/scripts/footer/footer.directive.js
+++ app/scripts/footer/footer.directive.js
@@ -1,6 +1,24 @@
-bulletApp.directive('footer', function(){
+bulletApp.directive('footer', function(currentStates, $state){
     return {
         restrict: 'E',
-        templateUrl: 'scripts/footer/footer.template.html'
+        templateUrl: 'scripts/footer/footer.template.html',
+        link: function (scope) {
+          scope.currentStates = currentStates
+
+          scope.lastMonth = function() {
+            if (currentStates.month) $state.go('month', currentStates.month)
+            else $state.go('month', {monthString: Moment().startOf('month').toISOString() })
+          }
+          scope.lastDaily = function() {
+            //currently does not use params
+            $state.go('daily') //, currentStates.day)
+          }
+          scope.lastFuture = function() {
+            $state.go('future', currentStates.future)
+          }
+          scope.lastGeneric = function() {
+            $state.go('generic', currentStates.generic)
+          }
+        }
     };
 });
