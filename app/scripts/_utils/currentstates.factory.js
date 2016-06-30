332d8687dc98d011311771c21744311089cb97e3
batmantis <batmantisex@gmail.com>
2016-06-30 16:17:01 -0400

    commented logs

--- app/scripts/_utils/currentstates.factory.js
+++ app/scripts/_utils/currentstates.factory.js
@@ -8,11 +8,11 @@ bulletApp.factory('currentStates', function ($rootScope) {
   }
   $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
     currentStates[toState.name] = toParams
-    // These are useful for testing
-    console.log('ts', toState);
-    console.log('tp', toParams);
-    console.log('fs', fromState);
-    console.log('fp', fromParams);
+    // // These are useful for testing
+    // console.log('ts', toState);
+    // console.log('tp', toParams);
+    // console.log('fs', fromState);
+    // console.log('fp', fromParams);
   });
 
   $rootScope.$on('pageChange', function(event, args){
@@ -23,11 +23,11 @@ bulletApp.factory('currentStates', function ($rootScope) {
 
   //This console logs if there are errors in a state change
   $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams){
-    console.log('err', event);
-    console.log('ts', toState);
-    console.log('tp', toParams);
-    console.log('fs', fromState);
-    console.log('fp', fromParams);
+    // console.log('err', event);
+    // console.log('ts', toState);
+    // console.log('tp', toParams);
+    // console.log('fs', fromState);
+    // console.log('fp', fromParams);
   });
 
 
