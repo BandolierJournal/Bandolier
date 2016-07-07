6e18f3bcb09ad41c864444df8b4668b3d6c2f6d8
sechu <sabrina.chu@gmail.com>
2016-07-07 16:14:56 -0400

    spent literally 2hrs formatting scheduler

--- app/scripts/bullets/bullet.directive.js
+++ app/scripts/bullets/bullet.directive.js
@@ -101,7 +101,6 @@ bulletApp.directive('bullet', function(DateFactory, $timeout, $rootScope) {
             element.on('keydown', function(e) {
                 if (e.which !== 9 && e.which !== 91) {
                     if (e.which === 13) {
-                        console.log(e);
                         e.preventDefault();
                         e.target.blur();
                     } else if ((OS === 'darwin' && e.metaKey) || (OS !== 'darwin' && e.ctrlKey)) {
@@ -119,7 +118,7 @@ bulletApp.directive('bullet', function(DateFactory, $timeout, $rootScope) {
                 $timeout(function() {
                     if (!scope.bullet.rev) scope.addFn();
                     else scope.bullet.save();
-                }, 100);
+                }, 5);
 
                 $timeout(function() {
                     scope.enableButtons = false;
--- app/scripts/index/landingpage.js
+++ app/scripts/index/landingpage.js
@@ -1,4 +1,5 @@
 bulletApp.controller('LandingCtrl', function($scope, $state, $timeout) {
- 
-    
+	$timeout(function() {
+		$state.go('index')
+	}, 2500);  
 });
