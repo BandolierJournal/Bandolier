3c5b5501199f744654bac019fd30ccd7d1d23c50
sechu <sabrina.chu@gmail.com>
2016-07-08 00:54:44 -0400

    daily search added

--- app/scripts/generic/generic.controller.js
+++ app/scripts/generic/generic.controller.js
@@ -2,5 +2,8 @@ bulletApp.controller('GenericCtrl', function($scope, collection, $state) {
     if (collection.type === 'month' || collection.type === 'month-cal') {
     	$state.go('month', { monthString: collection.title });
     }
+    if (collection.type === 'day') {
+    	$state.go('daily', {dayString: collection.title});
+    }
     $scope.collection = collection;
 });
--- app/scripts/log/daily.state.js
+++ app/scripts/log/daily.state.js
@@ -1,16 +1,13 @@
 bulletApp.config(function($stateProvider) {
 
     $stateProvider.state('daily', {
-        url: '/daily/:index',
+        url: '/daily/:index/:dayString',
         templateUrl: 'scripts/log/log.template.html',
         controller: 'LogCtrl',
         resolve: {
             collections: function(DateFactory) {
                 return Collection.fetchAll({ type: 'day' })
                     .then(DateFactory.splitCollections);
-            },
-            last: function($stateParams) {
-                return $stateParams.index || null;
             },
             type: () => 'day'
         }
